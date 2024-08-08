4769222e3e1814672fb40c0ca94de8c602bedfcc
Wad-Laptop <iamwad@gmail.com>
2024-08-08 13:15:29 +0700

    laser scanner with TextEditingController listener on TextChange

---
+++ android/app/src/main/kotlin/th/co/profile/laser_scanner1/MainActivity.kt
@@ -0,0 +1,5 @@
+package th.co.profile.laser_scanner1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/laser_scan_page.dart
@@ -0,0 +1,66 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class LaserScannerPage extends StatefulWidget {
+  final String title;
+
+  const LaserScannerPage({super.key , required this.title});
+
+  // @override
+  // _LaserScannerPageState createState() => _LaserScannerPageState();
+   @override
+  State<LaserScannerPage> createState() => _LaserScannerPageState();
+}
+
+class _LaserScannerPageState extends State<LaserScannerPage> {
+  final TextEditingController _controller = TextEditingController();
+  String _scannedCode = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(_onTextChanged);
+  }
+
+  void _onTextChanged() {
+    if (_controller.text.isNotEmpty) {
+      setState(() {
+        _scannedCode = _controller.text;
+        debugPrint('Scanned code: $_scannedCode');
+      });
+      _controller.clear();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Laser Scanner')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _controller,
+              autofocus: true,
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Scan a barcode',
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text('Scanned Code: $_scannedCode', style: const TextStyle(fontSize: 20)),
+          ],
+        ),
+      ),
+    );
+  }
+}
